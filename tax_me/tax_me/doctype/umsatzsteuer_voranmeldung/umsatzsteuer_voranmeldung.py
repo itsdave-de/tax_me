4dabcfc2f3c11ee36db4dc6c8319e86bd69d50d2
Beate Trzensiok <b.trzensiok@itsdave.de>
2021-09-28 16:40:28 +0200

    WIP

--- tax_me/tax_me/doctype/umsatzsteuer_voranmeldung/umsatzsteuer_voranmeldung.py
+++ tax_me/tax_me/doctype/umsatzsteuer_voranmeldung/umsatzsteuer_voranmeldung.py
@@ -3,11 +3,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from os import umask
 import frappe
 from frappe.model.document import Document
 from datetime import datetime as dt
-
+import io
+from frappe.utils.file_manager import save_file
 from frappe.utils import password
+from frappe.utils.data import encode
 import pymssql
 import re
 
@@ -15,11 +18,12 @@ class UmsatzsteuerVoranmeldung(Document):
 	#Regex für das Prüfen auf plausibles Datum
 	epic_date_regex = r"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$"
 
+	@frappe.whitelist()
 	def berechnen(self):
 		settings = frappe.get_single("Tax Me Einstellungen")
 
 		#Ausgangsbelege verarbeiten
-
+		
 		si_list = frappe.get_all(
 			"Sales Invoice", 
 			filters={
@@ -33,9 +37,11 @@ class UmsatzsteuerVoranmeldung(Document):
 		for si in si_list:
 			summe_netto += si["total"]
 			summe_steuern += si["total_taxes_and_charges"]
+			 
+
 		self.summe_netto = summe_netto
 		self.summe_umsatzsteuer = summe_steuern
-
+	
 		#Eingangsbelege verarbeiten
 
 		pi_list = self.get_eingangsrechnungen_from_inoxision(settings)
@@ -118,7 +124,48 @@ class UmsatzsteuerVoranmeldung(Document):
 				return_list.append(row)
 			
 		return return_list
-
+	
+	@frappe.whitelist()
+	def daten_generieren(self):
+		von_datum_dt = dt.strptime (self.von,"%Y-%m-%d")
+		von_datum = dt.strftime(von_datum_dt, "%Y%m%d")
+		bis_datum_dt = dt.strptime (self.bis,"%Y-%m-%d")
+		bis_datum = dt.strftime(bis_datum_dt, "%Y%m%d")
+		rechnungsdaten = "EXTF;510;21;Buchungsstapel;7;2,02E+16;;RE;;0084947SOL0000000022;84947;11155;"+von_datum+";4;"+von_datum+";"+bis_datum+"\n"+"Umsatz (ohne Soll/Haben-Kz);Soll/Haben-Kennzeichen;WKZ Umsatz;Kurs;Basis-Umsatz;WKZ Basis-Umsatz;Konto;Gegenkonto (ohne BU-Schlüssel);BU-Schlüssel;Belegdatum;Belegfeld 1;Belegfeld 2;Skonto;Buchungstext;;\n"
+		si_list = frappe.get_all(
+					"Sales Invoice", 
+					filters={
+						"docstatus": 1,
+						"posting_date": ["between", [self.von, self.bis]]
+						},
+					fields = ["name", "posting_date", "total", "is_return", "customer"]
+					)
+		for si in si_list:
+			if si ["is_return"] == 1:
+				soll_haben = "H"
+				buchungstext = "Rechnungskorrektur"
+			else:
+				soll_haben = "S"
+				buchungstext = "Rechnung"
+			
+			account = frappe.get_all("Party Account", filters={ "parent" : si["customer"]})
+			if len(account) ==1:
+				account_doc = frappe.get_doc("Party Account",account[0]["name"])
+			
+				konto = account_doc.debtor_creditor_number
+			umsatz = ("%.1f" % si["total"]).replace(".",",")
+			belegdatum_dt = dt.strptime (str(si["posting_date"]),"%Y-%m-%d")
+			belegdatum = dt.strftime(belegdatum_dt, "%d%m")
+			belegfeld1 = str(si["name"].split("-")[1])
+			
+			zeile = umsatz+";"+ soll_haben+";;;;;"+str(konto)+";4400;0;"+ belegdatum +";" + belegfeld1 + ";;;" + buchungstext +";;\n"
+			rechnungsdaten += zeile
+		print(rechnungsdaten)
+		b = io.BytesIO(rechnungsdaten.encode())
+		save_file("Ausgangsrechnungen_itsdave_gmbh.xlsx", b.read(),"Umsatzsteuer Voranmeldung",None,False,1)
+		# frappe.response["filename"] = "Ausgangsrechnungen_itsdave_gmbh.csv"
+		# frappe.response["filecontent"] = io_objekt
+		# frappe.response["type"] = "binary"
 
 
 
